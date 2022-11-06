53acc9247f271f2ee44c01866c0944319663a674
mjacoud <m.jacoud01@gmail.com>
2022-11-06 18:29:45 -0300

    v0.3
    -New Modal for edit tasks
    - Now you can delete tasks
    -  Toggle completion of tasks

--- todo/src/components/TaskList/index.tsx
+++ todo/src/components/TaskList/index.tsx
@@ -1,15 +1,24 @@
-import React from "react"
+import React from "react";
 
-import { Task } from "../Task"
+import { Task } from "../Task";
 
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
-export const TaskList = ({tasks}:any) => {
-
-  const taskTest = ({tasks}) => {tasks.map((tasks)=>{return console.log(tasks.id)})}
-
- return (
- <>
-  {tasks.map((tasks)=> <Task taskTest={taskTest} tasks={tasks} key={uuidv4()}/>) }
-</>
-)}
+export const TaskList = ({
+  tasks,
+  handleTaskStatus,
+  handleTaskDeletion,
+}: any) => {
+  return (
+    <>
+      {tasks.map((tasks) => (
+        <Task
+          tasks={tasks}
+          handleTaskStatus={handleTaskStatus}
+          handleTaskDeletion={handleTaskDeletion}
+          key={uuidv4()}
+        />
+      ))}
+    </>
+  );
+};
